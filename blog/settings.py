13fe8bf6ed89e3ce8fca1799ee9b7de3016caff3
raislansilva <raislan@live.com>
2020-11-30 14:10:25 -0500

    change static_root

--- blog/settings.py
+++ blog/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
-STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join('staticfile')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
